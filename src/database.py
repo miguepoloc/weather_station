a38918a69f66cbc22b0690b8d06326ca6ebb6378
miguepoloc <miguepoloc@gmail.com>
2024-04-29 22:25:33 -0500

    :sparkles: feat: add design pattern strategy to get nodes data value

--- src/database.py
+++ src/database.py
@@ -9,14 +9,14 @@ class Database:
     instance = None
 
     def __new__(cls):
-        DATABASE_URL = "postgresql://{}:{}@{}:{}/{}".format(
-            os.getenv("DB_USER"),
-            os.getenv("DB_PASS"),
-            os.getenv("DB_HOST"),
-            os.getenv("DB_PORT"),
-            os.getenv("DB_NAME"),
-        )
         if cls.instance is None:
+            DATABASE_URL = "postgresql://{}:{}@{}:{}/{}".format(
+                os.getenv("DB_USER"),
+                os.getenv("DB_PASS"),
+                os.getenv("DB_HOST"),
+                os.getenv("DB_PORT"),
+                os.getenv("DB_NAME"),
+            )
             cls.instance = super().__new__(cls)
             cls.instance.engine = create_engine(DATABASE_URL)
             cls.instance.session = sessionmaker(autocommit=False, autoflush=False, bind=cls.instance.engine)
