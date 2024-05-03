0f065054cda2fb0980a290333bba9f48635741c3
miguepoloc <miguepoloc@gmail.com>
2024-05-02 23:54:04 -0500

    :boom: feat: add ep for login and change password

---
+++ src/constants.py
@@ -0,0 +1,19 @@
+"""This module contains the constants used in the application."""
+
+import os
+
+
+class EnvironmentVariables:
+    """
+    This class represents the environment variables used in the application.
+
+    Note:
+        The values for these attributes are retrieved from environment variables using the `os.getenv` function.
+    """
+
+    SECRET_API_KEY: str = os.getenv("SECRET_API_KEY")  # type: ignore
+    TOKEN_API_EXPIRATION: int = int(os.getenv("TOKEN_API_EXPIRATION"))  # type: ignore
+    ENVIRONMENT: str = os.getenv("ENVIRONMENT")  # type: ignore
+    DEBUG: bool = os.getenv("DEBUG")  # type: ignore
+    AWS_S3_BUCKET_NAME: str = os.getenv("ENV_BUCKET_NAME")  # type: ignore
+    ALGORITHM: str = os.getenv("ALGORITHM")  # type: ignore
---
+++ src/repository.py
@@ -0,0 +1,10 @@
+"""This module contains the functions to interact with the database for the auth module."""
+
+from typing import Optional
+
+from models import User
+from sqlalchemy.orm import Session
+
+
+def get_user(db: Session, email: str) -> Optional[User]:
+    return db.query(User).filter(User.email == email).first()
