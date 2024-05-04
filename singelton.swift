6b50b043f1fcafccf258f5cf72496cf5a74537f7
Juancbernier <juanberniercr@unimagdalena.edu.co>
2024-05-03 19:32:37 -0500

    singleton pattern

---
+++ singelton.swift
@@ -0,0 +1,22 @@
+import Foundation
+
+//MARK: Creacion de un singleton
+ class Singleton {
+    static let sharedInstance = Singleton()
+    var param1: String?
+    var param2: String?
+    
+    private init(){
+        param1 = nil
+        param2 = nil
+    }
+    
+    func setParams(_ param1: String, _ param2: String){
+        self.param1 = param1
+        self.param2 = param2
+    }
+}
+
+//uso de singleton
+Singleton.sharedInstance.setParams("juan", "bernier")
+print(Singleton.sharedInstance.param1)
